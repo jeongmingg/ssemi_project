2a284a1bffa3049f4e82493da747750305f6b6b9
(남지혜) <works.jhnam@gmail.com>
2024-02-27 17:51:13 +0900

    로그인, 회원가입 기능 보수
    로그인, 회원가입 기능 보수

---
+++ matminProject/src/main/java/com/kh/member/controller/AjaxIdCheckController.java
@@ -0,0 +1,52 @@
+package com.kh.member.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.kh.member.model.service.MemberService;
+
+/**
+ * Servlet implementation class AjaxIdCheckController
+ */
+@WebServlet("/idCheck.me")
+public class AjaxIdCheckController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AjaxIdCheckController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String checkId = request.getParameter("checkId");
+		
+		int count = new MemberService().idCheck(checkId);
+		
+		if(count > 0) { // 존재하는 아이디가 있을 경우 => 사용 불가 => "NNNNN"
+			response.getWriter().print("NNNNN");
+		} else { // 존재하는 아이디가 없을 경우 => 사용 가능 => "NNNNY"
+			response.getWriter().print("NNNNY");
+		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
