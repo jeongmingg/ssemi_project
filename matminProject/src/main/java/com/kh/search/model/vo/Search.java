bb637e7e5f5add1d514d13425ccbeb6a10f81338
(정민) <ming2558@naver.com>
2024-02-28 11:15:52 +0900

    search keyword 성공

---
+++ matminProject/src/main/java/com/kh/search/model/vo/Search.java
@@ -0,0 +1,70 @@
+package com.kh.search.model.vo;
+
+public class Search {
+	
+	private String restNo;
+	private String restName;
+	private double restAvg;
+	private int heart;
+	private String restImgUrl;
+	
+	public Search() {}
+	
+	public Search(String restNo, String restName, double restAvg, int heart, String restImgUrl) {
+		super();
+		this.restNo = restNo;
+		this.restName = restName;
+		this.restAvg = restAvg;
+		this.heart = heart;
+		this.restImgUrl = restImgUrl;
+	}
+
+	public String getRestNo() {
+		return restNo;
+	}
+
+	public void setRestNo(String restNo) {
+		this.restNo = restNo;
+	}
+
+	public String getRestName() {
+		return restName;
+	}
+
+	public void setRestName(String restName) {
+		this.restName = restName;
+	}
+
+	public double getRestAvg() {
+		return restAvg;
+	}
+
+	public void setRestAvg(double restAvg) {
+		this.restAvg = restAvg;
+	}
+
+	public int getHeart() {
+		return heart;
+	}
+
+	public void setHeart(int heart) {
+		this.heart = heart;
+	}
+
+	
+	public String getRestImgUrl() {
+		return restImgUrl;
+	}
+
+	public void setRestImgUrl(String restImgUrl) {
+		this.restImgUrl = restImgUrl;
+	}
+
+	@Override
+	public String toString() {
+		return "Search [restNo=" + restNo + ", restName=" + restName + ", restAvg=" + restAvg + ", heart=" + heart
+				+ ", restImgUrl=" + restImgUrl + "]";
+	}
+
+	
+}
