98becc50425fec789163abf4e9c89e0289f0aa78
berrykiss <berrykiss@gmail.com>
2024-03-13 16:01:55 +0900

    comment

--- matminProject/src/main/java/com/kh/rest/model/service/RestService.java
+++ matminProject/src/main/java/com/kh/rest/model/service/RestService.java
@@ -107,12 +107,12 @@ public class RestService {
 		
 	}
 	
-	public ArrayList<Rest> locationSearch(String keyword, String locationName){
-		
-		Connection conn = getConnection();
-		ArrayList<Rest> lcList = new RestDao().locationSearch(conn, keyword, locationName);
-		
-		close(conn); 
-		return lcList;
-	}
+//	public ArrayList<Rest> locationSearch(String keyword, String locationName){
+//		
+//		Connection conn = getConnection();
+//		ArrayList<Rest> lcList = new RestDao().locationSearch(conn, keyword, locationName);
+//		
+//		close(conn); 
+//		return lcList;
+//	}
 }
